cf509231e658674739ab315ace8833ed8e65ed06
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-07-09 19:12:02 +0530

    added router |page not found

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-// import '../styles/globals.css'
 import "../styles/global.scss";
 import "antd/dist/antd.js";
 import type { AppProps } from "next/app";
-function MyApp({ Component, pageProps }: AppProps) {
+import Error from "./404";
+
+function MyApp({ Component, pageProps, err }: AppProps & { err: any }) {
+  if (err && err.code === 404) {
+    return <Error />;
+  }
+
   return <Component {...pageProps} />;
 }
 
